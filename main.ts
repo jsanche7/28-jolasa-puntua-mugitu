dce9fa84e3c460f9a8b0af6e90b5f5ebb203ae57
jsanche7 <80990132+jsanche7@users.noreply.github.com>
2021-11-11 22:44:58 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    Puntua.change(LedSpriteProperty.Y, 1)
+})
+input.onButtonPressed(Button.A, function () {
+    Puntua.move(-1)
+})
+input.onButtonPressed(Button.AB, function () {
+    Puntua.change(LedSpriteProperty.Y, -1)
+})
+input.onButtonPressed(Button.B, function () {
+    Puntua.move(1)
+})
+let Puntua: game.LedSprite = null
+Puntua = game.createSprite(2, 4)
